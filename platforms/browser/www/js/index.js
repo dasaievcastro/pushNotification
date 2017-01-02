85644fd258b384b28b5bfd6a8bca86e909af853a
Dasaiev de França Castro <dasaievcastroster@gmail.com>
2017-01-02 15:00:39 -0200

    Versão de testes
    testes do push

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+ function initPushwoosh() {
+    var pushwoosh = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+
+    // Should be called before pushwoosh.onDeviceReady
+    document.addEventListener('push-notification', function(event) {
+        var notification = event.notification;
+        console.log(notification)
+    // handle push open here
+    });
+
+    // Initialize Pushwoosh. This will trigger all pending push notifications on start.
+    pushwoosh.onDeviceReady({
+        appid: "EB8E3-B76E0",
+        projectid: "645474925587"
+    });
+
+    pushwoosh.registerDevice(
+        function(status) {
+        var pushToken = status.pushToken;
+        // handle successful registration here
+        },
+        function(status) {
+        // handle registration error here
+        }
+    );
+}
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        initPushwoosh();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
